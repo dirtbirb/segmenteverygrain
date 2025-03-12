46099d476e09b35e6bb63bfd880e3e5ff3bdc78e
Dave Matthews <DjmPhysics@gmail.com>
2025-03-12 15:26:16 -0700

    update examples

--- run_auto.py
+++ run_auto.py
@@ -13,11 +13,12 @@ logger = logging.getLogger(__name__)
 DBS_MAX_DIST = 20.0 # px
 I = 256             # px?
 MIN_AREA = 400      # px
-PX_PER_M = 1        # px/m
+PX_PER_M = 33.9     # px/m
 
 
-# Load test image
-fn = 'examples/torrey_pines_beach_image.jpeg'
+# Load image and define output filename stem
+fn = 'examples/torrey_pines.jpeg'
+out_fn = 'examples/auto/torrey_pines'
 logger.info(f'Loading image {fn}')
 image = si.load_image(fn)
 
@@ -34,12 +35,13 @@ unet_labels, unet_coords = segmenteverygrain.label_grains(
     image, unet_image, dbs_max_dist=DBS_MAX_DIST)
 
 # Save U-Net diagnostic image
+logger.info(f'Saving U-Net diagnostic image as {out_fn}')
 fig, ax = plt.subplots(figsize=(15,10))
 ax.imshow(unet_image)
 plt.scatter(np.array(unet_coords)[:,0], np.array(unet_coords)[:,1], c='k')
 plt.xticks([])
 plt.yticks([])
-fig.savefig('./output/test_unet.jpg', bbox_inches='tight', pad_inches=0)
+fig.savefig(out_fn + '_unet.jpg', bbox_inches='tight', pad_inches=0)
 plt.close()
 
 
@@ -52,7 +54,7 @@ predictor.set_image(image)
 
 # Apply SAM for actual segmentation
 logger.info(f'Segmenting image')
-grains, sam_labels, mask, summary, fig, ax = segmenteverygrain.sam_segmentation(
+polygons, sam_labels, mask, summary, fig, ax = segmenteverygrain.sam_segmentation(
     sam, image, unet_image, unet_coords, unet_labels,
     min_area=MIN_AREA,
     plot_image=True,
@@ -61,22 +63,27 @@ grains, sam_labels, mask, summary, fig, ax = segmenteverygrain.sam_segmentation(
 
 
 # Extract results
-grains = [si.Grain(np.array(g.exterior.xy)) for g in grains]
+logger.info(f'Measuring grains')
+grains = si.polygons_to_grains(polygons)
 for g in tqdm(grains):
     g.measure(image=image)
-fn = './output/test_auto'
-logger.info(f'Saving results as {fn}')
+
+
+# Save results
+# NOTE: U-Net image was already saved, in case of error during SAM step
+# NOTE: out_fn was defined at image import step
+logger.info(f'Saving results as {out_fn}')
 # Grain shapes
-si.save_grains(fn + '_grains.geojson', grains)
+si.save_grains(out_fn + '_grains.geojson', grains)
 # Grain image
-fig.savefig(fn + '_grains.jpg', bbox_inches='tight', pad_inches=0)
+fig.savefig(out_fn + '_grains.jpg', bbox_inches='tight', pad_inches=0)
 # Summary data
-si.save_summary(fn + '_summary.csv', grains, px_per_m=PX_PER_M)
+si.save_summary(out_fn + '_summary.csv', grains, px_per_m=PX_PER_M)
 # Summary histogram
-si.save_histogram(fn + '_summary.jpg', grains, px_per_m=PX_PER_M)
+si.save_histogram(out_fn + '_summary.jpg', grains, px_per_m=PX_PER_M)
 # Training mask
-si.save_mask(fn + '_mask.png', grains, image, scale=False)
-si.save_mask(fn + '_mask2.jpg', grains, image, scale=True)
+si.save_mask(out_fn + '_mask.png', grains, image, scale=False)
+si.save_mask(out_fn + '_mask2.jpg', grains, image, scale=True)
 
 
 logger.info(f'Auto-segmenting complete!')
--- run_grid_count.py
+++ run_grid_count.py
@@ -8,28 +8,30 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-FIGURE_DPI = 72     # dots/inch
-PX_PER_M = 1        # px/m
-SPACING = 220       # px
+FIGURE_DPI = 72                     # dots/inch
+PX_PER_M = 33.9                     # px/m
+SPACING_M = 0.155                   # m
+SPACING_PX = PX_PER_M / SPACING_M   # px
 
 
 # Load image
-fn = 'examples/torrey_pines_beach_image.jpeg'
+fn = 'examples/torrey_pines.jpeg'
 logger.info(f'Loading image {fn}')
 image = si.load_image(fn)
 img_y, img_x = image.shape[:2]
 
 # Load grains
-fn = 'examples/test_edit_grains.geojson'
+fn = 'examples/interactive/torrey_pines_grains.geojson'
 logger.info(f'Loading grains from {fn}')
 grains = si.load_grains(fn)
 
 
 # Find and measure grains according to a grid
-logger.info('Picking grains at grid locations')
-points, xs, ys = si.make_grid(image, SPACING)
+logger.info('Constructing grid')
+points, xs, ys = si.make_grid(image, SPACING_PX)
+logger.info(f'Generated {len(points)} measurement points.')
 grains, points_found = si.filter_grains_by_points(grains, points)
-logger.info('Measuring picked grains')
+logger.info('Measuring selected grains')
 for g in tqdm(grains):
     g.measure(image=image)
 
@@ -37,9 +39,10 @@ for g in tqdm(grains):
 # Get GrainPlot as a static image
 logger.info('Creating plot')
 plot = si.GrainPlot(grains, image, 
-    figsize=(img_x/FIGURE_DPI, img_y/FIGURE_DPI), 
-    dpi=FIGURE_DPI,
-    image_alpha=0.5)
+    figsize = (img_x / FIGURE_DPI, img_y / FIGURE_DPI), 
+    dpi = FIGURE_DPI,
+    px_per_m = PX_PER_M,
+    image_alpha = 0.5)
 plot_image = np.asarray(plot.canvas.buffer_rgba(), dtype=np.uint8)
 
 # Make new plot using static GrainPlot image as background
@@ -52,7 +55,6 @@ ax.autoscale(enable=False)
 
 # Plot grain axes
 for grain in tqdm(grains):
-    grain.rescale(1 / plot.scale)
     grain.draw_axes(ax)
 
 # Plot grid
@@ -63,9 +65,10 @@ ax.scatter(xs, ys,
 
 
 # Save results
-fn = './output/test_count'
+fn = 'examples/grid_count/torrey_pines'
 logger.info(f'Saving results as {fn}')
 # Grain shapes
+grains = plot.grains
 si.save_grains(fn + '_grains.geojson', grains)
 # Grain image
 fig.savefig(fn + '_grains.jpg', bbox_inches='tight', pad_inches=0.2)
